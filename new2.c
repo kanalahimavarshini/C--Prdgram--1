09e77564464706f2d4e07e56a7ffa27f6201ff28
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-03 19:30:04 +0530

    new3.c

--- new2.c
+++ new2.c
@@ -15,3 +15,21 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    float radius, area;
+    float pi = 3.14159;
+
+    // Input radius
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    // Calculate area
+    area = pi * radius * radius;
+
+    // Display result
+    printf("Area of the circle = %.2f", area);
+
+    return 0;
+}
