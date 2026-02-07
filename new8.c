d14be1a6f2c284bf8b86a834e0cdb0bc2f1f3048
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-07 10:19:53 +0530

    new8.c

---
+++ new8.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int a[5], b[5], c[5], i;
+
+    // Read first array
+    printf("Enter 5 elements of first array:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Read second array
+    printf("Enter 5 elements of second array:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &b[i]);
+    }
+
+    // Add arrays and store in third array
+    for (i = 0; i < 5; i++) {
+        c[i] = a[i] + b[i];
+    }
+
+    // Print result
+    printf("Sum of two arrays:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", c[i]);
+    }
+
+    return 0;
+}
