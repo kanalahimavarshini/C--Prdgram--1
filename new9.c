4575ab0c10d1d8410e70cbba5caa6a100f6407f2
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-08 11:44:36 +0530

    new9A.c

--- new9.c
+++ new9.c
@@ -12,3 +12,17 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 1; i <= 10; i++) {
+        if (i == 5) {
+            break;
+        }
+        printf("%d ", i);
+    }
+
+    return 0;
+}
