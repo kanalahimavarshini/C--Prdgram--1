2d751a20697c8d10b4f9e32f9a3632b3c05001ec
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-11 16:48:23 +0530

    new12A.c

--- new12.c
+++ new12.c
@@ -18,3 +18,23 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    float num1, num2, result;
+
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    if (num2 != 0) {
+        result = num1 / num2;
+        printf("Division of %.2f by %.2f is: %.2f\n", num1, num2, result);
+    } else {
+        printf("Error! Division by zero is not allowed.\n");
+    }
+
+    return 0;
+}
