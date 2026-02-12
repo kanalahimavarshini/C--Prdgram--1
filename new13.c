15692f24e9bed66072b8b65ef25c5d94019aecd7
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-12 21:04:34 +0530

    hlo

---
+++ new13.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float
+    a,b.c,real,imag;
+    float
+    root1,root2,d;
+    printf("enter the values of co-efficient a,b,c in a single line with space in between :\n");
+    scanf("%f%f%f",&a,&b,&c);
+    d=((b*b)-(4*a*c));
+    if (d==0)
+    {
+    printf("the roots are real and equal\n");
+    root1=root2=((-b)/(2*a));
+    printf("root1=root2 is %.3f\n",root1);
+    }
+    else if(d>0)
+    { 
+        printf("the roots are real and distinct\n");
+        root1=(((-b)+sqrt(d)/2*a));
+        root2=(((-b)-sqrt(d)/2*a));
+        printf("root1 is %.3f\n root 2 is %.3f \n",root1,root2);
+    }
+    else
+    {
+        printf("the roots are real and imaginary\n");
+        real=(-b)/(2*a);
+        imag=sqrt(fabs(d))/(2*a);
+        printf("root1 is %.3f+i%.3f\n",real,imag);
+        printf("root2 is %.3f-i%.3f\n",real,imag);    
+    }
+}
