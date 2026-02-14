f76dcceafc2f40a3fa9e8bab8203030102d519a9
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-14 22:58:35 +0530

    hii

--- new2.c
+++ new2.c
@@ -33,3 +33,16 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int i, sum = 0;
+
+    for (i = 1; i <= 10; i++) {
+        sum = sum + i;
+    }
+
+    printf("Sum of first 10 natural numbers = %d", sum);
+
+    return 0;
+}
