64598266e49eb3c8aaaeff7e990c463d91a202db
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-15 21:51:34 +0530

    hii2

--- new3.c
+++ new3.c
@@ -63,3 +63,15 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int i = 1;   // initialization
+
+    while (i <= 5) {   // condition
+        printf("%d\n", i);
+        i++;   // increment
+    }
+
+    return 0;
+}
