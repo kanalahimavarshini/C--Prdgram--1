5f85d8aba2ca8161cf11ab1e24fa4429d895ecb6
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-16 22:36:21 +0530

    division1

--- new4.c
+++ new4.c
@@ -44,3 +44,19 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    float num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    if (num2 == 0) {
+        printf("Division by zero is not allowed.");
+    } else {
+        printf("Result = %.2f", num1 / num2);
+    }
+
+    return 0;
+}
