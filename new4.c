13b0726009c6aca6ed9f551d2cb93b4eb698b3ee
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-16 22:37:06 +0530

    division2

--- new4.c
+++ new4.c
@@ -60,3 +60,20 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    if (b == 0) {
+        printf("Division by zero is not allowed.");
+    } else {
+        printf("Quotient = %d\n", a / b);
+        printf("Remainder = %d", a % b);
+    }
+
+    return 0;
+}
