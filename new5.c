7e02f96d052885828a07dbdbe3df578753202041
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-17 22:33:31 +0530

    integer

--- new5.c
+++ new5.c
@@ -13,3 +13,19 @@ int main()
     printf("After swapping x=%d,y=%d",x,y);
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Integers from 1 to %d are:\n", n);
+
+    for (i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
