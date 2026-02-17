fb09878fe993b74add5cd643a9d608c502df537c
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-17 22:39:41 +0530

    student

--- new5.c
+++ new5.c
@@ -167,3 +167,30 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+int main() {
+    struct Student s;
+
+    printf("Enter Name: ");
+    scanf("%s", s.name);
+
+    printf("Enter Roll Number: ");
+    scanf("%d", &s.roll);
+
+    printf("Enter Marks: ");
+    scanf("%f", &s.marks);
+
+    printf("\nStudent Details:\n");
+    printf("Name: %s\n", s.name);
+    printf("Roll: %d\n", s.roll);
+    printf("Marks: %.2f\n", s.marks);
+
+    return 0;
+}
