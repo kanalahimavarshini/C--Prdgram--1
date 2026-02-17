7b35976f564d71e2fb5ed3d96ea75cbb53d24a88
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-17 22:39:13 +0530

    string

--- new5.c
+++ new5.c
@@ -147,3 +147,23 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, length;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    length = strlen(str);
+
+    printf("Reversed string: ");
+
+    for(i = length - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
