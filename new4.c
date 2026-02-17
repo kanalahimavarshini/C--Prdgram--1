46f1777108760879dc275f08e06ed3e5cedea8fa
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-17 22:45:07 +0530

    swapping

--- new4.c
+++ new4.c
@@ -112,3 +112,26 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    swap(&a, &b);
+
+    printf("After swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    return 0;
+}
