b333f9c67c95fab1361dddedc989941e94eab682
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-17 22:38:32 +0530

    small or large

--- new5.c
+++ new5.c
@@ -121,3 +121,29 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int arr[5], i;
+    int largest, smallest;
+
+    printf("Enter 5 integers:\n");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    largest = smallest = arr[0];
+
+    for(i = 1; i < 5; i++) {
+        if(arr[i] > largest)
+            largest = arr[i];
+
+        if(arr[i] < smallest)
+            smallest = arr[i];
+    }
+
+    printf("Largest = %d\n", largest);
+    printf("Smallest = %d\n", smallest);
+
+    return 0;
+}
