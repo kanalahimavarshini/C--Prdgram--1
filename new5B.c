265984892d777748e36e2b7edcc341a75b67e3e6
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-18 21:56:07 +0530

    hlo

--- new5B.c
+++ new5B.c
@@ -41,3 +41,31 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+int main() {
+    struct Student s;
+    struct Student *ptr = &s;
+
+    printf("Enter name: ");
+    scanf("%s", ptr->name);
+
+    printf("Enter roll: ");
+    scanf("%d", &ptr->roll);
+
+    printf("Enter marks: ");
+    scanf("%f", &ptr->marks);
+
+    printf("\nStudent Details:\n");
+    printf("Name: %s\n", ptr->name);
+    printf("Roll: %d\n", ptr->roll);
+    printf("Marks: %.2f\n", ptr->marks);
+
+    return 0;
+}
