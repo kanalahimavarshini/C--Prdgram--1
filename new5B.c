984be4094a9665c38a367ffa9b7ad958096c05b4
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-18 21:55:12 +0530

    hii

--- new5B.c
+++ new5B.c
@@ -9,3 +9,35 @@ int main()
     printf("%d %d",a,b);
     return 0;
 }
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, i;
+    int *ptr;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    // allocate memory
+    ptr = (int*)malloc(n * sizeof(int));
+
+    if(ptr == NULL) {
+        printf("Memory not allocated!\n");
+        return 1;
+    }
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &ptr[i]);
+    }
+
+    printf("You entered:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", ptr[i]);
+    }
+
+    free(ptr);   // free memory
+
+    return 0;
+}
