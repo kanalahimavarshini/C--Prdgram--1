cded0433eaa81a27885bdb4bd4f65cd21c8b08e0
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-18 22:00:02 +0530

    hlo2

--- new5B.c
+++ new5B.c
@@ -145,3 +145,48 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+int main() {
+    struct Node *head = NULL, *temp, *newNode;
+    int n, i;
+
+    printf("How many nodes? ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        // Allocate memory
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+
+        printf("Enter data: ");
+        scanf("%d", &newNode->data);
+
+        newNode->next = NULL;
+
+        if(head == NULL) {
+            head = temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Display Linked List
+    printf("Linked List: ");
+    temp = head;
+
+    while(temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL");
+
+    return 0;
+}
