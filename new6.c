8b5806e61523e98a3a8e0eb0eda719f4b20ea7ca
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-19 21:13:43 +0530

    function

--- new6.c
+++ new6.c
@@ -203,3 +203,44 @@ int main()
 
     return 0;
 }
+#include <stdio.h>
+
+// Function for Linear Search
+int linearSearch(int arr[], int n, int key)
+{
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] == key)
+        {
+            return i;   // return index if found
+        }
+    }
+    return -1;  // return -1 if not found
+}
+
+int main()
+{
+    int arr[100], n, i, key, result;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    result = linearSearch(arr, n, key);
+
+    if(result != -1)
+        printf("Element found at position %d", result + 1);
+    else
+        printf("Element not found");
+
+    return 0;
+}
