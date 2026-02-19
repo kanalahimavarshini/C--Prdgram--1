3c423dd774742ec84fdd3031a14eca067ded5f80
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-19 21:16:25 +0530

    linear function

--- new7.c
+++ new7.c
@@ -50,3 +50,44 @@ int main()
     scanf("%d", &n);
 
     printf("Enter %d elements (in sorted order
+#include <stdio.h>
+
+// Function definition
+int linearSearch(int arr[], int n, int key)
+{
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] == key)
+        {
+            return i;   // return index if found
+        }
+    }
+    return -1;   // return -1 if not found
+}
+
+int main()
+{
+    int arr[100], n, i, key, pos;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    pos = linearSearch(arr, n, key);
+
+    if(pos != -1)
+        printf("Element found at position %d", pos + 1);
+    else
+        printf("Element not found");
+
+    return 0;
+}
