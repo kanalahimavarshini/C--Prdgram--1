6dad2fd58099df16561fc7811a717c0f71127e6c
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-19 21:19:39 +0530

    hlo

--- new7.c
+++ new7.c
@@ -129,4 +129,54 @@ int main()
 
     return 0;
 }
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i, key;
+    int low = 0, high, mid;
+    int *ptr;
+    int found = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements (in sorted order):\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    ptr = arr;        // pointer to first element
+    high = n - 1;
+
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if(*(ptr + mid) == key)
+        {
+            found = 1;
+            break;
+        }
+        else if(*(ptr + mid) < key)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    if(found == 1)
+        printf("Element found at position %d", mid + 1);
+    else
+        printf("Element not found");
+
+    return 0;
+}
 
