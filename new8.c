74feca7a59473fcccdb96cd8c12e165e5b138b1f
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-20 20:32:03 +0530

    large

--- new8.c
+++ new8.c
@@ -28,3 +28,22 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int largest(int a, int b)
+{
+    if(a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    int x, y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Largest = %d", largest(x, y));
+    return 0;
+}
