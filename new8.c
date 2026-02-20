9d96bbdb97ef177188a25fc45b5d3020020198ea
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-20 20:37:17 +0530

    struc1

--- new8.c
+++ new8.c
@@ -127,3 +127,30 @@ int main()
 
     return 0;
 }
+#include <stdio.h>
+
+struct student
+{
+    int id;
+    float marks;
+};
+
+int main()
+{
+    struct student s[3];
+    int i;
+
+    for(i = 0; i < 3; i++)
+    {
+        printf("Enter ID and Marks: ");
+        scanf("%d %f", &s[i].id, &s[i].marks);
+    }
+
+    printf("\nStudent Details:\n");
+    for(i = 0; i < 3; i++)
+    {
+        printf("ID = %d  Marks = %.2f\n", s[i].id, s[i].marks);
+    }
+
+    return 0;
+}
