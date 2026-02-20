ba6d908b47e93316dd7d1349017dfff30646c0e3
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-20 20:36:30 +0530

    student

--- new8.c
+++ new8.c
@@ -98,3 +98,32 @@ int main()
     printf("Reverse = %d", reverse(num));
     return 0;
 }
+#include <stdio.h>
+
+struct student
+{
+    int id;
+    char name[20];
+    float marks;
+};
+
+int main()
+{
+    struct student s;
+
+    printf("Enter ID: ");
+    scanf("%d", &s.id);
+
+    printf("Enter Name: ");
+    scanf("%s", s.name);
+
+    printf("Enter Marks: ");
+    scanf("%f", &s.marks);
+
+    printf("\nStudent Details:\n");
+    printf("ID = %d\n", s.id);
+    printf("Name = %s\n", s.name);
+    printf("Marks = %.2f\n", s.marks);
+
+    return 0;
+}
