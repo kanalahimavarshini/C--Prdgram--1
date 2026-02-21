79727fde2d5b9d5690e8e1ffe40d74fba7cafdc8
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-21 21:53:49 +0530

    matrix

--- new9.c
+++ new9.c
@@ -185,3 +185,62 @@ int main()
 
     return 0;
 }
+#include <stdio.h>
+
+void multiply(int a[10][10], int b[10][10], int c[10][10], int r1, int c1, int c2)
+{
+    int i, j, k;
+
+    for(i = 0; i < r1; i++)
+    {
+        for(j = 0; j < c2; j++)
+        {
+            c[i][j] = 0;
+            for(k = 0; k < c1; k++)
+            {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
+
+int main()
+{
+    int a[10][10], b[10][10], c[10][10];
+    int r1, c1, r2, c2;
+    int i, j;
+
+    printf("Enter rows and columns of first matrix: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter rows and columns of second matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    if(c1 != r2)
+    {
+        printf("Matrix multiplication not possible");
+        return 0;
+    }
+
+    printf("Enter first matrix:\n");
+    for(i = 0; i < r1; i++)
+        for(j = 0; j < c1; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Enter second matrix:\n");
+    for(i = 0; i < r2; i++)
+        for(j = 0; j < c2; j++)
+            scanf("%d", &b[i][j]);
+
+    multiply(a, b, c, r1, c1, c2);
+
+    printf("Result matrix:\n");
+    for(i = 0; i < r1; i++)
+    {
+        for(j = 0; j < c2; j++)
+            printf("%d ", c[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
