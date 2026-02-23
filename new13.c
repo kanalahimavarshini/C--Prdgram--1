10a4d02e158e19eae548574e13bae6ade8902c1c
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-23 21:25:26 +0530

    dsa2

--- new13.c
+++ new13.c
@@ -190,3 +190,44 @@ int main() {
     display();
     return 0;
 }
+#include <stdio.h>
+#define MAX 5
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+void enqueue(int value) {
+    if(rear == MAX - 1) {
+        printf("Queue Overflow\n");
+    } else {
+        if(front == -1) front = 0;
+        rear++;
+        queue[rear] = value;
+    }
+}
+
+void dequeue() {
+    if(front == -1 || front > rear) {
+        printf("Queue Underflow\n");
+    } else {
+        printf("Deleted %d\n", queue[front]);
+        front++;
+    }
+}
+
+void display() {
+    for(int i = front; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    enqueue(5);
+    enqueue(10);
+    enqueue(15);
+    display();
+    dequeue();
+    display();
+    return 0;
+}
