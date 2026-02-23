ddea2966d9d3c800fe10bf9750233c3af54d3db4
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-23 23:16:07 +0530

    array3

--- new2.c
+++ new2.c
@@ -172,3 +172,38 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int arr[5], i;
+    int *ptr = arr;
+
+    printf("Enter 5 elements:\n");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", ptr + i);
+    }
+
+    printf("Array elements are:\n");
+    for(i = 0; i < 5; i++) {
+        printf("%d ", *(ptr + i));
+    }
+
+    return 0;
+}#include <stdio.h>
+
+int main() {
+    int arr[5], i;
+    int *ptr = arr;
+
+    printf("Enter 5 elements:\n");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", ptr + i);
+    }
+
+    printf("Array elements are:\n");
+    for(i = 0; i < 5; i++) {
+        printf("%d ", *(ptr + i));
+    }
+
+    return 0;
+}
