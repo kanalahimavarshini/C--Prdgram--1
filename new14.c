8d2b986ab771ad44374337de36cfb47a750c71c3
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-24 23:16:30 +0530

    dsa7

--- new14.c
+++ new14.c
@@ -188,3 +188,22 @@ int main() {
     printf("Second largest = %d", second);
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int n = 29, flag = 0;
+
+    for(int i = 2; i <= n/2; i++) {
+        if(n % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if(flag == 0)
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
