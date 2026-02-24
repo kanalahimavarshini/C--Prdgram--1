6a13e6bebfbecfcb9dd0142bffa43c354e46fc6c
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-24 13:24:12 +0530

    dsa2

--- new14.c
+++ new14.c
@@ -36,3 +36,17 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int arr[] = {12, 45, 7, 89, 34};
+    int max = arr[0];
+
+    for(int i = 1; i < 5; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    printf("Largest element = %d", max);
+    return 0;
+}
