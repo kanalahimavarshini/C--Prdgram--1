a2538e317d40d42946942d4900bb8f5cddd0c468
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-24 23:13:29 +0530

    dsa3

--- new14.c
+++ new14.c
@@ -102,3 +102,25 @@ int main() {
     pop();
     return 0;
 }
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "madam";
+    int n = strlen(str);
+    int flag = 1;
+
+    for(int i = 0; i < n/2; i++) {
+        if(str[i] != str[n-i-1]) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
