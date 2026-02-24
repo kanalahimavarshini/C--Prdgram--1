20be8e3a1b5d3193c61820b47491e999de729662
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-24 13:25:40 +0530

    dsa3

--- new14.c
+++ new14.c
@@ -50,3 +50,25 @@ int main() {
     printf("Largest element = %d", max);
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int arr[] = {5, 3, 8, 2, 1};
+    int n = 5;
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array:\n");
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
