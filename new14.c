112f2b60ac6f470c2be564c6fd2e69330c4b48dd
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-24 23:14:00 +0530

    dsa4

--- new14.c
+++ new14.c
@@ -124,3 +124,16 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int fib(int n) {
+    if(n <= 1)
+        return n;
+    return fib(n-1) + fib(n-2);
+}
+
+int main() {
+    int n = 6;
+    for(int i = 0; i < n; i++)
+        printf("%d ", fib(i));
+}
