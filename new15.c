81157f04706b4b41f51a747443424257405af90f
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-25 20:49:01 +0530

    dsa2

--- new15.c
+++ new15.c
@@ -31,3 +31,44 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int n, i, key;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    int *ptr = arr;
+
+    printf("Enter sorted elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", ptr + i);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    int low = 0, high = n - 1, mid, found = 0;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+
+        if(*(ptr + mid) == key) {
+            found = 1;
+            break;
+        }
+        else if(*(ptr + mid) < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    if(found)
+        printf("Element found at position %d", mid + 1);
+    else
+        printf("Element not found");
+
+    return 0;
+}
