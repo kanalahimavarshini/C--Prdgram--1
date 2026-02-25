df7ab34aaee326615279eaa5edce67f0a3fac5a0
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-25 20:52:33 +0530

    pointers1

--- new15.c
+++ new15.c
@@ -203,3 +203,26 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    int *ptr = arr;
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", ptr + i);
+    }
+
+    printf("Array in reverse order:\n");
+    for(i = n - 1; i >= 0; i--) {
+        printf("%d ", *(ptr + i));
+    }
+
+    return 0;
+}
