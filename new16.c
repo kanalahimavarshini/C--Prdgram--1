e52f4a9652ed1cdb5f2721235db95ac2885d9504
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-28 21:09:20 +0530

    dsa4

--- new16.c
+++ new16.c
@@ -79,3 +79,37 @@ int main() {
     }
     return 0;
 }
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+int main() {
+    struct node *head = NULL, *temp, *newnode;
+    int n = 3;
+
+    for(int i=0;i<n;i++) {
+        newnode = (struct node*)malloc(sizeof(struct node));
+        printf("Enter data: ");
+        scanf("%d",&newnode->data);
+        newnode->next = NULL;
+
+        if(head == NULL) {
+            head = temp = newnode;
+        } else {
+            temp->next = newnode;
+            temp = newnode;
+        }
+    }
+
+    temp = head;
+    while(temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
