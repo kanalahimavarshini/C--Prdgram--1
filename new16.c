6551e8b53705cdc46ce544f5c0662061c7893176
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-28 21:11:59 +0530

    dsa8

--- new16.c
+++ new16.c
@@ -190,3 +190,23 @@ int main() {
     printf("No pair found");
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1,1,2,2,3,4,4};
+    int n = 7;
+
+    int i = 0;
+
+    for(int j=1;j<n;j++) {
+        if(arr[i] != arr[j]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+
+    for(int k=0;k<=i;k++)
+        printf("%d ", arr[k]);
+
+    return 0;
+}
