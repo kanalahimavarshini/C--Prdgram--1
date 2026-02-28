01ab472b7d4f648951392d6560932983cea76c0d
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-28 21:11:23 +0530

    dsa7

--- new16.c
+++ new16.c
@@ -164,3 +164,29 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1,2,3,4,6,8};
+    int target = 10;
+    int n = 6;
+
+    int left = 0;
+    int right = n - 1;
+
+    while(left < right) {
+        int sum = arr[left] + arr[right];
+
+        if(sum == target) {
+            printf("Pair: %d %d", arr[left], arr[right]);
+            return 0;
+        }
+        else if(sum < target)
+            left++;
+        else
+            right--;
+    }
+
+    printf("No pair found");
+    return 0;
+}
