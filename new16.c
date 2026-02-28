c62ee36743015297e16ed1f1fea4e3bfc41ec043
Himavarshini <kanalahimavarshini@gmail.com>
2026-02-28 21:10:31 +0530

    dsa5

--- new16.c
+++ new16.c
@@ -113,3 +113,26 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+    int n = 5;
+
+    int left = 0;
+    int right = n - 1;
+
+    while(left < right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    for(int i=0;i<n;i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
