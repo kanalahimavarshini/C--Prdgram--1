88f70e3bf45b744d9e4c7d29fb0451af57bfa997
Himavarshini <kanalahimavarshini@gmail.com>
2026-03-01 21:22:19 +0530

    dynamic prob2

--- new17.c
+++ new17.c
@@ -156,3 +156,32 @@ int main() {
 
     printf("LCS Length = %d", dp[m][n]);
 }
+#include <stdio.h>
+#include <limits.h>
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int main() {
+    int coins[] = {1, 2, 5};
+    int amount = 11;
+    int n = 3;
+
+    int dp[100];
+
+    dp[0] = 0;
+
+    for (int i = 1; i <= amount; i++)
+        dp[i] = INT_MAX;
+
+    for (int i = 1; i <= amount; i++) {
+        for (int j = 0; j < n; j++) {
+            if (coins[j] <= i && dp[i - coins[j]] != INT_MAX) {
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+    }
+
+    printf("Minimum coins = %d", dp[amount]);
+}
