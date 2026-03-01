99b5763d9738fc7c35cd8b60af596c6930acb148
Himavarshini <kanalahimavarshini@gmail.com>
2026-03-01 21:20:28 +0530

    dynamic prob

--- new17.c
+++ new17.c
@@ -78,3 +78,25 @@ int main() {
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
 }
+#include <stdio.h>
+
+int fib(int n, int dp[]) {
+    if (n <= 1)
+        return n;
+
+    if (dp[n] != -1)
+        return dp[n];
+
+    dp[n] = fib(n-1, dp) + fib(n-2, dp);
+    return dp[n];
+}
+
+int main() {
+    int n = 10;
+    int dp[100];
+
+    for (int i = 0; i <= n; i++)
+        dp[i] = -1;
+
+    printf("Fibonacci = %d", fib(n, dp));
+}
