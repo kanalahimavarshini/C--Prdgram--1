dbf8bcfb432d8c1e82878b531d5f50a353dd8bec
Himavarshini <kanalahimavarshini@gmail.com>
2026-03-01 21:21:02 +0530

    dynamic prob1

--- new17.c
+++ new17.c
@@ -100,3 +100,30 @@ int main() {
 
     printf("Fibonacci = %d", fib(n, dp));
 }
+#include <stdio.h>
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int main() {
+    int wt[] = {1, 3, 4, 5};
+    int val[] = {1, 4, 5, 7};
+    int W = 7;
+    int n = 4;
+
+    int dp[10][10];
+
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (wt[i-1] <= w)
+                dp[i][w] = max(val[i-1] + dp[i-1][w - wt[i-1]], dp[i-1][w]);
+            else
+                dp[i][w] = dp[i-1][w];
+        }
+    }
+
+    printf("Maximum value = %d", dp[n][W]);
+}
