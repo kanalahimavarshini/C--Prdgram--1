11461134ef61f83666207216f996d767e98d3f77
Himavarshini <kanalahimavarshini@gmail.com>
2026-03-01 21:24:53 +0530

    max and min

--- new17.c
+++ new17.c
@@ -209,3 +209,23 @@ int main() {
 
     printf("Length of LIS = %d", max);
 }
+#include <stdio.h>
+
+int main() {
+    int arr[] = {12, 45, 7, 89, 23};
+    int n = 5;
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Maximum = %d\n", max);
+    printf("Minimum = %d", min);
+}
