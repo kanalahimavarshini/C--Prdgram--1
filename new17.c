80627b2179ffdfe76d1f3e90df01a107f0438347
Himavarshini <kanalahimavarshini@gmail.com>
2026-03-01 21:23:00 +0530

    dynamic prob3

--- new17.c
+++ new17.c
@@ -185,3 +185,27 @@ int main() {
 
     printf("Minimum coins = %d", dp[amount]);
 }
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 22, 9, 33, 21, 50};
+    int n = 6;
+
+    int dp[100];
+    int max = 1;
+
+    for (int i = 0; i < n; i++)
+        dp[i] = 1;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                dp[i] = dp[j] + 1;
+                if (dp[i] > max)
+                    max = dp[i];
+            }
+        }
+    }
+
+    printf("Length of LIS = %d", max);
+}
