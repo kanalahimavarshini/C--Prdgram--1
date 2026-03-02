377e1be14561b2e2e1a0ebd11461ae26327a9fd9
Himavarshini <kanalahimavarshini@gmail.com>
2026-03-02 22:40:53 +0530

    hlo

--- new17.c
+++ new17.c
@@ -250,3 +250,24 @@ int main() {
 
     printf("Maximum Subarray Sum = %d", max_sum);
 }
+include <stdio.h>
+
+int main() {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = 9;
+
+    int max_sum = arr[0];
+    int current_sum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (current_sum + arr[i] > arr[i])
+            current_sum = current_sum + arr[i];
+        else
+            current_sum = arr[i];
+
+        if (current_sum > max_sum)
+            max_sum = current_sum;
+    }
+
+    printf("Maximum Subarray Sum = %d", max_sum);
+}
