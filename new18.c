622521592e625779b8445bbdd35ce6a235cd2e51
Himavarshini <kanalahimavarshini@gmail.com>
2026-03-03 22:01:37 +0530

    prob1

--- new18.c
+++ new18.c
@@ -18,3 +18,32 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int sumArray(int *arr, int n) {
+    int i, sum = 0;
+    for(i = 0; i < n; i++) {
+        sum += *(arr + i);
+    }
+    return sum;
+}
+
+int main() {
+    int n, i;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int result = sumArray(arr, n);
+
+    printf("Sum = %d", result);
+
+    return 0;
+}
