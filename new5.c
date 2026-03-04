a28903f06d52eaedcc5e6de73e776f7ed89edb6d
Himavarshini <kanalahimavarshini@gmail.com>
2026-03-04 18:48:47 +0530

    prob

--- new5.c
+++ new5.c
@@ -223,3 +223,20 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int arr[6];
+    int i, count = 0;
+
+    printf("Enter 6 integers:\n");
+
+    for(i = 0; i < 6; i++) {
+        scanf("%d", &arr[i]);
+        count++;
+    }
+
+    printf("Total integers entered = %d\n", count);
+
+    return 0;
+}
